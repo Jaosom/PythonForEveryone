492d7929a7d3344f4e4ad8f6de74edc5ac5a896a
Jaosom <92562301+Jaosom@users.noreply.github.com>
2024-09-19 16:57:17 +0700

    เพิ่ม read write csv
    เพิ่มหน้าต่าง flash card

--- GUI-knowledge.py
+++ GUI-knowledge.py
@@ -2,8 +2,20 @@
 from tkinter import * # * คือ import all function inside main package
 from tkinter import ttk
 import csv
+import os
+
+#os.getcwd cwd = current working directory
+path = os.getcwd()
+
+
+noteicon = os.path.join(path,'noteicon.ico')
+
 
 def writecsv(data):
+    #csvfile ตัวแปรที่เก็บ os.path คือที่อยู่ของ file
+    #join เชื่อมต่อ path,แล้วก้อ file 'knowledge.csv'
+    #ใช้เครื่องอื่นจะไม่ error 
+    csvfile = os.path.join(path,'knowledge.csv')
     # data = data มีลักษณะเป็น list ['john', 14, 500]
     # เปิด หรือสร้าง knowledge.csv 'a'ทำต่อกันไปเรื่อยๆ
     # ถ้าเป็น 'w' จะเป็นการทำครั้งเดียวเหมือนเขียนทับ
@@ -16,9 +28,14 @@ def writecsv(data):
 GUI = Tk() #Tk เป็น software หลัก เราจะดึงความสามารถของ Tk มาใช้
 GUI.title('โปรแกรมบันทึกความรู้')
 GUI.geometry('500x500')
+GUI.iconbitmap(noteicon) #ใส่ รูป icon ในหัวโปรแกรม
 
-L1 = ttk.Label(GUI,text='หัวข้อความรู้',font=('Angsana New',16))
-L1.pack()
+F1 = Frame(GUI) #เอา GUI ไปใส่ในเฟรม ในตัวแปรชื่อ F1
+F1.place(x=20,y=50)
+
+#ตอนแรกใส่เป็น GUI ให้เปลี่ยนเป็น F1 ทั้งหมดเพราะเราเอาไปใส่ใน Frame แล้ว
+L1 = ttk.Label(F1,text='หัวข้อความรู้',font=('Angsana New',16))
+L1.pack()#pady = ระยะห่างของแกน y
 
 
 
@@ -27,16 +44,16 @@ L1.pack()
 v_title = StringVar()
 
 #ช่องกรอกข้อมูล width คือ กรอกได้กี่ตัวอักษร height คือ กี่บรรทัด
-E1 = ttk.Entry(GUI,textvariable=v_title,font=('Angsana New',20),width=50)
+E1 = ttk.Entry(F1,textvariable=v_title,font=('Angsana New',20),width=50)
 #grid = เหมือนตารางไว้ในตารางไหน 
 #pack = อยู่ตรงกลาง
 #place = กำหนดค่า x,y
 E1.pack()
 
-L2 = ttk.Label(GUI,text='รายละเอียด',font=('Angsana New',16))
+L2 = ttk.Label(F1,text='รายละเอียด',font=('Angsana New',16))
 L2.pack()
 
-T1 = Text(GUI,font=('Angsana New',18),height=8,width=56)
+T1 = Text(F1,font=('Angsana New',18),height=8,width=56)
 T1.pack()
 
 def save():
@@ -54,9 +71,81 @@ def save():
     T1.delete('1.0',END) #clear text box
 
 #command=save ให้กดปุ่มแล้ว save
-B1 = ttk.Button(GUI,text='บันทึก',command=save)
+B1 = ttk.Button(F1,text='บันทึก',command=save)
 B1.pack(pady=10,ipadx=20,ipady=10)
 
+
+######################### Button Flash Card #################
+
+def readcsv():
+    with open('knowledge.csv',newline='',encoding='utf8') as file:
+        fr = csv.reader(file)
+        data = list(fr)
+        return data  ##เราจะไม่ print data แต่จะใช้ return แทน
+
+knowledgelist = readcsv()
+print(knowledgelist)
+
+global countindex
+countindex = 0
+
+#สร้าง function ให้มันโชว์หน้าต่าง โชว์ความรู้ขึ้นมา
+def Flashcard():
+    knowledgelist = readcsv() 
+    #ปิดหน้าต่าง flashcard แล้วเพิ่มข้อมูลจะได้ไม่ต้องปิดโปรแกรมแล้วเปิดใหม่
+    GUI2 = Toplevel()
+    GUI2.title('ทบทวนความรู้')
+    GUI2.geometry('500x400')
+
+
+    vtext_title = StringVar()
+    vtext_detail = StringVar()
+    title = ttk.Label(GUI2,textvariable=vtext_title,font=('Angsana New',20,'bold'))
+    title.pack()
+    vtext_title.set(knowledgelist[0][0])
+    detail = ttk.Label(GUI2,textvariable=vtext_detail,font=('Angsana New',14))
+    detail.pack()
+    vtext_detail.set(knowledgelist[0][1].replace('\r',''))
+    
+
+    def Prev():
+        global countindex
+        if countindex == 0:
+            countindex = countindex
+        else:
+            countindex = countindex - 1
+
+        #set text
+        vtext_title.set(knowledgelist[countindex][0])
+        vtext_detail.set(knowledgelist[countindex][1].replace('\r',''))
+
+    def Next():
+        global countindex
+        if countindex == (len(knowledgelist)-1):
+            countindex = len(knowledgelist)-1
+        else:
+            countindex = countindex + 1
+
+        #set text
+        vtext_title.set(knowledgelist[countindex][0])
+        vtext_detail.set(knowledgelist[countindex][1].replace('\r',''))
+
+    BPrev = ttk.Button(GUI2,text='<',command=Prev)
+    BPrev.place(x=170,y=350)
+    BNext = ttk.Button(GUI2,text='>',command=Next)
+    BNext.place(x=250,y=350)
+    
+
+    GUI2.mainloop()
+
+
+
+notebutton = os.path.join(path,'note.png')
+noteicon = PhotoImage(file=notebutton) 
+
+#กด icon แล้วจะมีหน้าต่างใหม่ขึ้นมาที่เราสร้างเป็น GUI2 ที่อยู่ใน function Flashcard
+BFlashcard = ttk.Button(GUI,image=noteicon,command=Flashcard)
+BFlashcard.place(x=440,y=20)
 
 GUI.mainloop() #mainloop คือทำให้ program run ตลอดโดยไม่ต้องปิดโปรแกรม
 
